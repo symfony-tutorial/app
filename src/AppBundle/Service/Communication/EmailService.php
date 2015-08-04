e2f8d3975c11905d22fbed87c402084e3bdbf9e2
skafandri <you@example.com>
2015-08-04 07:31:03 +0300

    12.2

--- src/AppBundle/Service/Communication/EmailService.php
+++ src/AppBundle/Service/Communication/EmailService.php
@@ -4,6 +4,7 @@ namespace AppBundle\Service\Communication;
 
 use AppBundle\Communication\Email\Message;
 use AppBundle\Communication\Email\ProviderInterface;
+use AppBundle\Service\UtilService;
 
 class EmailService
 {
@@ -13,21 +14,34 @@ class EmailService
     private $providers = array();
     private $providerIndex = -1;
 
-    public function addProvider(ProviderInterface $provider)
-    {
+    /**
+     *
+     * @var UtilService
+     */
+    private $utilService;
+    private $maxPerSecond;
+
+    function __construct(UtilService $utilService, $maxPerSecond) {
+        $this->utilService = $utilService;
+        $this->maxPerSecond = $maxPerSecond;
+    }
+
+    public function addProvider(ProviderInterface $provider) {
         $this->providers[] = $provider;
     }
 
-    public function send(Message $message)
-    {
+    public function send(Message $message) {
+        $this->utilService->enterConcurrentSection('email', $this->maxPerSecond, 1);
+
         $this->incrementIndex();
         $provider = $this->providers[$this->providerIndex];
+        $result = $provider->send($message);
 
-        return $provider->send($message);
+        $this->utilService->exitConcurrentSection('email');
+        return $result;
     }
 
-    private function incrementIndex()
-    {
+    private function incrementIndex() {
         $this->providerIndex++;
         if ($this->providerIndex > count($this->providers) - 1) {
             $this->providerIndex = 0;
--- src/AppBundle/Service/UtilService.php
+++ src/AppBundle/Service/UtilService.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Service;
 
+use AppBundle\Exception\ConcurrentAccessException;
 use AppBundle\Exception\LockException;
+use Memcached;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Symfony\Bridge\Monolog\Logger;
 
@@ -22,21 +24,24 @@ class UtilService
      * @var Logger
      */
     private $logger;
-    
-    private $prefix;
 
-    function __construct(RegistryInterface $registry, Logger $logger, $prefix = 'app.') {
+    /**
+     *
+     * @var Memcached
+     */
+    private $memcached;
+
+    function __construct(RegistryInterface $registry, Logger $logger, Memcached $memcached) {
         $this->registry = $registry;
         $this->logger = $logger;
-        $this->prefix = $prefix;
+        $this->memcached = $memcached;
     }
 
     public function getLock($lock, $timeout = 10) {
-        $lock = $this->prefix.$lock;
         $this->logger->addInfo(sprintf('acquiring named lock "%s"', $lock));
         $connection = $this->registry->getConnection();
         $result = $connection->executeQuery(
-            "SELECT GET_LOCK(':lock', :timeout)", array('lock' => $lock, 'timeout' => $timeout)
+                "SELECT GET_LOCK(':lock', :timeout)", array('lock' => $lock, 'timeout' => $timeout)
         );
         if (!$result) {
             throw new LockException(sprintf('Cannot get named lock %s', $lock));
@@ -45,12 +50,26 @@ class UtilService
     }
 
     public function releaseLock($lock) {
-        $lock = $this->prefix.$lock;
         $connection = $this->registry->getConnection();
         $connection->executeQuery(
                 "SELECT RELEASE_LOCK(':lock')", array('lock' => $lock)
         );
         $this->logger->addInfo(sprintf('released named lock "%s"', $lock));
+    }
+
+    public function enterConcurrentSection($resource, $limit, $timeLimit) {
+        $key = sprintf('limit_concurrents.%s', $resource);
+        $this->memcached->add($key, 0, $timeLimit);
+        if ($this->memcached->increment($key) > $limit) {
+            $this->memcached->decrement($key);
+            throw new ConcurrentAccessException(
+            sprintf('Only %s processes can access %s per %s seconds', $limit, $resource, $timeLimit)
+            );
+        }
+    }
+
+    public function exitConcurrentSection($resource) {
+        $this->memcached->decrement(sprintf('limit_concurrents.%s', $resource));
     }
 
 }
